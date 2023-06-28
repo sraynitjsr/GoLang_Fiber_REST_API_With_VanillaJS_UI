1a3412a6dd43c380a26ed2e3ec2554f3318f9db9
unknown <subhradeepray2017@gmail.com>
2023-06-28 20:04:37 +0530

    REST API With Graceful Shutdown

--- main.go
+++ main.go
@@ -1,51 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-var users = []User{
-	{ID: 1, Name: "Subhradeep Ray"},
-	{ID: 2, Name: "Sachin Tendulkar"},
-}
-
-func home(c *fiber.Ctx) error {
-	welcome := "Welcome to Users REST API"
-	return c.SendString(welcome)
-}
-
-func getUsers(c *fiber.Ctx) error {
-	return c.JSON(users)
-}
-
-func getUser(c *fiber.Ctx) error {
-	id := c.Params("id")
-
-	for _, user := range users {
-		if fmt.Sprint(user.ID) == id {
-			return c.JSON(user)
-		}
-	}
-
-	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-		"message": "User not found",
-	})
+func rootHandler(c *fiber.Ctx) error {
+	return c.SendString("Welcome to the API!")
 }
 
 func main() {
+	// Create a new Fiber app
 	app := fiber.New()
 
-	app.Get("/", home)
-	app.Get("/users", getUsers)
-	app.Get("/users/:id", getUser)
+	// Middleware
+	app.Use(logger.New())
 
-	log.Fatal(app.Listen(":8000"))
+	// Routes
+	app.Get("/", rootHandler)
+
+	// Create a channel to listen for an interrupt or termination signal from the OS
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	// Start the server in a separate goroutine
+	go func() {
+		if err := app.Listen(":3000"); err != nil {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	// Wait for the interrupt signal
+	<-quit
+	log.Println("Shutting down server...")
+
+	// Create a context with a timeout for the graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Perform the graceful shutdown by shutting down the server and blocking until all connections are closed
+	if err := app.Shutdown(ctx); err != nil {
+		log.Fatalf("Graceful shutdown error: %v", err)
+	}
+
+	log.Println("Server gracefully stopped")
 }
