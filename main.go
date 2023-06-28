b56cacd1a92e382ff60b6d411308ca17810dd6dd
unknown <subhradeepray2017@gmail.com>
2023-06-28 19:53:45 +0530

    Adding home, users, userById Controllers

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+var users = []User{
+	{ID: 1, Name: "Subhradeep Ray"},
+	{ID: 2, Name: "Sachin Tendulkar"},
+}
+
+func home(c *fiber.Ctx) error {
+	welcome := "Welcome to Users REST API"
+	return c.SendString(welcome)
+}
+
+func getUsers(c *fiber.Ctx) error {
+	return c.JSON(users)
+}
+
+func getUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	for _, user := range users {
+		if fmt.Sprint(user.ID) == id {
+			return c.JSON(user)
+		}
+	}
+
+	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+		"message": "User not found",
+	})
+}
 
 func main() {
-	fmt.Println("REST API Using GoLang Fiber")
+	app := fiber.New()
+
+	app.Get("/", home)
+	app.Get("/users", getUsers)
+	app.Get("/users/:id", getUser)
+
+	log.Fatal(app.Listen(":8000"))
 }
