a82bef655096377705ca1b0c9f9d14298b4bedb7
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-08-26 10:19:10 +0530

    Custom Go Fiber API Rate Limiter

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-    "github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/middleware/limiter"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func main() {
-  app := fiber.New()
+	app := fiber.New()
 
-  app.Use(limiter.New())
+	app.Use(limiter.New(limiter.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.IP() == "127.0.0.1"
+		},
+		Max:        20,
+		Expiration: 30 * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.Get("x-forwarded-for")
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.SendFile("./toofast.html")
+		},
+		Storage: myCustomStorage{},
+	}))
 
-  app.Get("/", func(c *fiber.Ctx) {
-    c.Send("Hello, World!")
-  })
+	app.Get("/", func(c *fiber.Ctx) {
+		c.Send("Hello, World!")
+	})
 
-  app.Listen(3000)
+	app.Listen(3000)
 }
