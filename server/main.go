c7b46bd4a479b88f0c44fca6fc189d1d98f0e6f8
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-06 23:19:19 +0530

    Separating Handlers From main()

--- server/main.go
+++ server/main.go
@@ -7,28 +7,36 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
+func welcomeHandler(c *fiber.Ctx) error {
+	return c.SendString("Welcome to Items Web Application")
+}
+
+func getItemAHandler(c *fiber.Ctx) error {
+	data := map[string]string{
+		"message": "Item A Fetched",
+	}
+	return c.JSON(data)
+}
+
+func getItemBHandler(c *fiber.Ctx) error {
+	data := map[string]string{
+		"message": "Item B Fetched",
+	}
+	return c.JSON(data)
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Use(cors.New())
+
+	app.Get("/", welcomeHandler)
+	app.Get("/getItemA", getItemAHandler)
+	app.Get("/getItemB", getItemBHandler)
+}
+
 func main() {
 	app := fiber.New()
 
-	app.Use(cors.New())
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Welcome to Items Web Application")
-	})
-
-	app.Get("/getItemA", func(c *fiber.Ctx) error {
-		data := map[string]string{
-			"message": "Item A Fetched",
-		}
-		return c.JSON(data)
-	})
-
-	app.Get("/getItemB", func(c *fiber.Ctx) error {
-		data := map[string]string{
-			"message": "Item B Fetched",
-		}
-		return c.JSON(data)
-	})
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
