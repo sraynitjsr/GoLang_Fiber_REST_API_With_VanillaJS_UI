3fce637bf560042148b329208ae43da27f087359
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-06 23:38:29 +0530

    Loose Coupling main.go

--- server/main.go
+++ server/main.go
@@ -5,38 +5,13 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"yourappname/handler"
 )
-
-func welcomeHandler(c *fiber.Ctx) error {
-	return c.SendString("Welcome to Items Web Application")
-}
-
-func getItemAHandler(c *fiber.Ctx) error {
-	data := map[string]string{
-		"message": "Item A Fetched",
-	}
-	return c.JSON(data)
-}
-
-func getItemBHandler(c *fiber.Ctx) error {
-	data := map[string]string{
-		"message": "Item B Fetched",
-	}
-	return c.JSON(data)
-}
-
-func setupRoutes(app *fiber.App) {
-	app.Use(cors.New())
-
-	app.Get("/", welcomeHandler)
-	app.Get("/getItemA", getItemAHandler)
-	app.Get("/getItemB", getItemBHandler)
-}
 
 func main() {
 	app := fiber.New()
 
-	setupRoutes(app)
+	handler.SetupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
