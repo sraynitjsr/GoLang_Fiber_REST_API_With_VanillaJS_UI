0927fee7b02f7e8be45ce58b66e6d2b20479e84d
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-12-21 23:49:39 +0530

    Getting all items in sorted order

--- server/utility/utility.go
+++ server/utility/utility.go
@@ -1,1 +1,17 @@
 package utility
+
+import (
+	"github.com/sraynitjsr/server/db"
+	"github.com/sraynitjsr/server/model"
+	"sort"
+)
+
+func GetSortedItems(itemDB *db.ItemDB) []model.ItemStruct {
+	allItems := itemDB.GetAllItems()
+  
+	sort.Slice(allItems, func(i, j int) bool {
+		return allItems[i].ID < allItems[j].ID
+	})
+  
+	return allItems
+}
