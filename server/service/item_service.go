6c5c6290379d4c1bfe0b561e180cb39dd8dc5bb4
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-12-21 23:55:12 +0530

    Adding delete methods for item service

--- server/service/item_service.go
+++ server/service/item_service.go
@@ -6,4 +6,6 @@ type ItemService interface {
 	GetItemByID(id int) (model.ItemStruct, bool)
 	GetItemByName(name string) (model.ItemStruct, bool)
 	GetAllItems() []model.ItemStruct
+	DeleteItemByID(id int) bool
+	DeleteItemByName(name string) bool
 }
